41913920e5387500acd1ec62c9dd252c3f4f8e5f
Ada Jensy <adajensy@outlook.com>
2020-06-09 15:00:48 -0600

    Estoy escriendo el codigo const morgan y app

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require("express");
 require('dotenv').config()
+const morgan = require('morgan');
 
-// inicializar servidor
 const app = express();
 
-// settings
 app.set("port",process.env.PORTSERVER || 3000)
 
-// middleware
+app.use(morgan('dev'));
 
-//ruotes
-
-//Static files
-
-//Elserver escuche
 app.listen(app.get('port'), () => {
     console.log("Server start on port");
   });
